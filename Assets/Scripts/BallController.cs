4c55cfef5133ca10d33b05ca52a32f54d0ec507e
JNeto <joaomendes15@gmail.com>
2023-02-16 09:32:22 +0000

    checkpoint for changes to be made

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,19 +6,44 @@ using Random = UnityEngine.Random;
 public class BallController : MonoBehaviour {
     
     private Rigidbody _rgb;
-    private Vector3 _direction = new Vector3(-1,0,0);
-    private const float ReviveThrust = 10;
+    
+    private float Speed = 800f;
+    private const float MinXAxisNorm = 0.4f;
+    
     private const float LaunchThrust = 60;
     private const float FirstLaunchThrust = 30;
+    private const float ReviveThrust = 10;
     private const float JumpThrust = 20;
 
     private void Start() {
         this._rgb = GetComponent<Rigidbody>();
-        this._direction.x = Random.Range(0, 2) == 0 ? -1 : 1;
-        this._direction.z = Random.Range(0, 2) == 0 ? -1 : 1;
-        this.Launch(FirstLaunchThrust);
+        
+        Vector3 initialDir = new Vector3();
+        initialDir.x = Random.Range(0, 2) == 0 ? -1 : 1;
+        initialDir.z = Random.Range(0, 2) == 0 ? -1 : 1;
+        this.Launch(initialDir.normalized, FirstLaunchThrust);
     }
+    
+    private void FixedUpdate() {
 
+        Vector3 dir = this._rgb.velocity.normalized;
+        
+        // avoinding ball to get stuck
+        if (dir.x < 0 && dir.x > - MinXAxisNorm) {
+            dir.x = - MinXAxisNorm;
+            dir = dir.normalized;
+        }
+        else if (dir.x > 0 && dir.x < MinXAxisNorm) {
+            dir.x = MinXAxisNorm;
+            dir = dir.normalized;
+        }
+        this._rgb.velocity = this.Speed * Time.deltaTime * dir;
+        
+        // sometimes the launch doesnt work, and the ball stops on the floor, so I just give it another push
+        if (this._rgb.velocity == Vector3.zero) 
+            this.Launch(this._rgb.velocity.normalized, ReviveThrust);
+    }
+    
     private void OnCollisionEnter(Collision collision) {
         
         // Score Zone
@@ -30,34 +55,21 @@ public class BallController : MonoBehaviour {
             Destroy(this.gameObject);
         }
         
-        // Wall: simply updates the direction
+        // Wall
         else if (collision.gameObject.tag.Equals("Wall")) {
             Debug.Log("Wall hit");
-            this._direction.z *= -1;
-         
         }
         
         // Player
         else if (collision.gameObject.tag.Equals("Player")) {
             Debug.Log("Player hit");
-            this._direction.x *= -1;
-            this.Launch(LaunchThrust);
         }
 
     }
-    
-    // sometimes the launch doesnt work, and the ball stops on the floor,
-    // so I just give it another push
-    private void Update() {
-        if (this._rgb.velocity == Vector3.zero) 
-            this.Launch(ReviveThrust);
-    }
-    
-    
-    private void Launch(float thrust) {
-        _direction.Normalize();
-        this._rgb.velocity = _direction * thrust;
-        this._rgb.AddForce(Vector3.up * JumpThrust);
+
+    private void Launch(Vector3 direction, float thrust) {
+        this._rgb.velocity = direction * thrust;
+        //this._rgb.AddForce(Vector3.up * JumpThrust);
     }
     
 }
